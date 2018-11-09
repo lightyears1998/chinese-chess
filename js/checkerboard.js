aab2b789e93ef09c8b126d0e01e3a0e7192ef39b
DangoSky <1341514988@qq.com>
2018-11-09 13:32:24 +0800

    写好了棋子并完成了响应式设计

--- js/checkerboard.js
+++ js/checkerboard.js
@@ -1,18 +1,32 @@
 // 绘制棋盘
-function checkerboard() {	
+function checkerboard() {	// 画线
+	drawCheckerboard();     // 画棋盘
+	drawChess(4, 9);            // 画棋子
+	drawChess(3, 9);
+}
+// 画线
+function cross(sx, sy, ex, ey, bw) {
+	context.beginPath();
+	context.moveTo(sx+paddingX, sy+paddingY);
+	context.lineTo(ex+paddingX, ey+paddingY);
+	context.strokeStyle =  "red";
+	context.lineWidth = bw;
+	context.stroke();
+	context.closePath();
+}
+// 画棋盘
+function drawCheckerboard() {
 	// 外框
- 	context.beginPath();
+	context.beginPath();
 	context.moveTo(0,0);
 	context.lineTo(canvasWidth,0);
 	context.lineTo(canvasWidth, canvasHeight);
 	context.lineTo(0, canvasHeight);	
 	context.lineTo(0,0);
 	context.lineWidth = 6;
-	context.strokeStyle= "red";
+	context.strokeStyle = "red";
 	context.stroke();
 	context.closePath(); 
-	var ceilWidth = Math.ceil(boxWidth / 8);    // 棋盘上每个格子的宽
-	var ceilHeight = Math.ceil(boxHeight / 9);  // 棋盘上每个格子的高
 	for(var i=0; i<=9; i++) {        // 横线
 		cross(0, i*ceilHeight, ceilWidth*8, i*ceilHeight, 3);
 	}
@@ -60,7 +74,7 @@ function checkerboard() {
 			cross(ceilWidth*i+disX, ceilHeight*3+disY, ceilWidth*i+disX, ceilHeight*3+disY+lineH, 1);
 		}
 	}
-	// 下期盘炮折线
+	// 下棋盘炮折线
 	for(var i=1; i<=7; i+=6){
 		cross(ceilWidth*i-disX-lineW, ceilHeight*7-disY, ceilWidth*i-disX, ceilHeight*7-disY, 1);    // 上棋盘左炮折线，按左上右上右下左下的顺序
 		cross(ceilWidth*i-disX, ceilHeight*7-disY, ceilWidth*i-disX, ceilHeight*7-disY-lineH, 1);
@@ -86,32 +100,31 @@ function checkerboard() {
 			cross(ceilWidth*i+disX, ceilHeight*6+disY, ceilWidth*i+disX, ceilHeight*6+disY+lineH, 1);
 		}
 	}
-	context.beginPath();
-	context.arc(8*ceilWidth+paddingX, 9*ceilHeight+paddingY, ceilWidth*0.37, 0, 2*Math.PI);
-	context.closePath();
-	context.stroke();
-/* 	context.beginPath();
-	context.arc(7*ceilWidth+paddingX,9*ceilHeight+paddingY,ceilWidth*0.37,0,2*Math.PI);
-	context.closePath();
-	context.stroke();
-	context.beginPath();
-	context.arc(7*ceilWidth+paddingX,8*ceilHeight+paddingY,ceilWidth*0.37,0,2*Math.PI);
-	context.closePath();
-	context.stroke(); */
-	
-	//context.font = "fontSize KaiTi";
-	context.font = "45px KaiTi";
+	// 楚河汉界
+	var fontSize = ceilHeight / 1.5;
+	context.font = String(fontSize) + 'px' + " KaiTi";
 	context.fillStyle = "red";
-	context.fillText("楚河",ceilWidth*1,ceilHeight*5);
-	context.fillText("汉界",ceilWidth*7,ceilHeight*5);
+	context.fillText("楚河", ceilWidth*0.8, ceilHeight*5+paddingY/2);
+	context.fillText("汉界", ceilWidth*6.8, ceilHeight*5+paddingY/2);
 }
-// 画线
-function cross(sx, sy, ex, ey, bw) {
-	context.beginPath();
-	context.moveTo(sx+paddingX, sy+paddingY);
-	context.lineTo(ex+paddingX, ey+paddingY);
-	context.strokeStyle =  "red";
-	context.lineWidth = bw;
-	context.stroke();
-	context.closePath();
+// 画棋子
+function drawChess(obj) {
+	if(obj !== 0){
+			context.beginPath();
+			context.strokeStyle = "rgb(221, 200, 113)";
+			context.fillStyle = "rgb(221, 200, 113)";
+			context.arc(obj.y*ceilWidth+paddingX, obj.x*ceilHeight+paddingY, chessSize, 0, 2*Math.PI);
+			context.fill();
+			if(obj.group === "red") {
+				context.fillStyle = "red";
+			}
+			if(obj.group === "black") {
+				context.fillStyle = "black";
+			}
+			context.font = String(chessFontSize) + 'px' + "KaiTi";
+			context.fillText(obj.name, obj.y*ceilWidth+paddingX/2.8, obj.x*ceilHeight+paddingY*1.5);
+			context.stroke();
+			context.closePath();
+	}
+
 }
--- js/main.js
+++ js/main.js
@@ -1,20 +1,64 @@
-<<<<<<< HEAD
-// 布置画布
-var canvas = document.getElementById("canvas");
+var canvas = document.getElementById("canvas");  // 布置画布
 var context = canvas.getContext("2d");
 var canvasWidth, canvasHeight;  // 画布大小
 var boxHeight, boxWidth;  // 棋盘大小
 var paddingX, paddingY;   // 棋盘距离外框的距离
+var ceilWidth, ceilHeight;  // 格子大小
+var chessSize;   // 棋子大小
+var chessFontSize;  // 棋子文字大小
+var mark = new Array(10);   // 标记棋盘上该位置是否有棋子
+ // 棋子对象
+var redKing = new Chess("red", "帅", 0, 4);
+	redGuard1 = new Chess("red", "士", 0, 3);
+	redGuard2 = new Chess("red", "士", 0, 5);
+	redBishop1 = new Chess("red", "象", 0, 2);
+	redBishop2 = new Chess("red", "象", 0, 6);
+	redKnight1 = new Chess("red", "马", 0, 1);
+	redKnight2 = new Chess("red", "马", 0,7);
+	redRook1 = new Chess("red", "车", 0, 0);
+	redRook2 = new Chess("red", "车", 0, 8);
+	redCannon1 = new Chess("red", "炮", 2, 1);
+	redCannon2 = new Chess("red", "炮", 2, 7);
+	redPawn1 = new Chess("red", "兵", 3, 0);
+	redPawn2 = new Chess("red", "兵", 3, 2);
+	redPawn3 = new Chess("red", "兵", 3, 4);
+	redPawn4 = new Chess("red", "兵", 3, 6);
+	redPawn5 = new Chess("red", "兵", 3, 8);
+var blackKing = new Chess("black", "将", 9, 4);
+	blackGuard1 = new Chess("black", "士", 9, 3);
+	blackGuard2 = new Chess("black", "士", 9, 5);
+	blackBishop1 = new Chess("black", "象", 9, 2);
+	blackBishop2 = new Chess("black", "象", 9, 6);
+	blackKnight1 = new Chess("black", "马", 9, 1);
+	blackKnight2 = new Chess("black", "马", 9,7);
+	blackRook1 = new Chess("black", "车", 9, 0);
+	blackRook2 = new Chess("black", "车", 9, 8);
+	blackCannon1 = new Chess("black", "炮", 7, 1);
+	blackCannon2 = new Chess("black", "炮", 7, 7);
+	blackPawn1 = new Chess("black", "卒", 6, 0);
+	blackPawn2 = new Chess("black", "卒", 6, 2);
+	blackPawn3 = new Chess("black", "卒", 6, 4);
+	blackPawn4 = new Chess("black", "卒", 6, 6);
+	blackPawn5 = new Chess("black", "卒", 6, 8);
 $(function(){
 	responisive();     // 响应式设计
 	checkerboard();    // 绘制棋盘
+	init();
 	// 实时监听浏览器宽高变化
 	$(window).resize(function() {
 		responisive();
 		checkerboard();
+		init();
 	});
 });
-
+// 棋子的构造函数
+function Chess(group, name, x, y) {
+	this.group = group;
+	this.name= name;
+	this.x = x;
+	this.y = y;
+}
+// 响应式设计
 function responisive() {
 	// 获取浏览器可使用的宽高
 	var browserWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
@@ -25,58 +69,60 @@ function responisive() {
 	canvas.width = Math.ceil(minLength / 9 * 8);
 	canvasWidth = canvas.width;
 	canvasHeight = canvas.height;
-	paddingX =  canvasWidth * 0.066;
-	paddingY =  canvasHeight * 0.059;
-	boxWidth = canvasWidth - paddingX*2;
-	boxHeight = canvasHeight -paddingY*2 ;
-	$(".box").width(canvasWidth).height(canvasHeight)
-   // alert(canvasWidth + " " + canvasHeight);
-	
+	ceilWidth = canvasWidth / 9;
+	ceilHeight = canvasHeight / 10;
+	paddingX =  ceilWidth / 2;
+	paddingY =  ceilHeight / 2;
+	boxWidth = canvasWidth - paddingX * 2;
+	boxHeight = canvasHeight - paddingY * 2 ;
+	chessSize = Math.ceil(ceilWidth * 0.4);
+	chessFontSize = Math.ceil(chessSize*0.8);
+	$(".box").width(canvasWidth).height(canvasHeight);
 }
-=======
-var context;
-var boxWidth, boxHeight;
-
-// 计算绘图所需的参数
-function calculateArgs() {
-	// 获取浏览器可使用宽高
-	var browserWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-	var browserHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-	// 布置画布
-	var canvas = document.getElementById("canvas");
-	context = canvas.getContext("2d");
-	// 响应式设计宽高
-	var maxLength = Math.min(browserWidth, browserHeight) * 0.8;  // 设定canvas的最大长度为浏览器的宽高中较小值的80% 
-	canvas.height = Math.ceil(maxLength);
-	canvas.width = Math.ceil(maxLength / 9 * 8);
-	boxHeight = canvas.height;
-	boxWidth = canvas.width;
-	
-	/*
-	if(browserWidth / browserHeight >= 1){       // 当前使用电脑，根据高度来设置棋盘宽高
-		canvas.height = ;
-		boxHeight = canvas.height;
-		canvas.width = Math.ceil(boxHeight * 8 / 9);
-		boxWidth = canvas.width;
-	}	
-	else {                       // 当前使用手机，根据宽度来设置棋盘宽高
-		canvas.width = Math.ceil(browserWidth * 8 / 9);
-		boxWidth = canvas.width;
-		canvas.higth = Math.ceil(boxWidth * 9 / 8);
-		boxHeight = canvas.height;
-	}*/
-	
-	$(".box").width(boxWidth).height(boxHeight);
+// 初始化mark数组
+function init() {
+	for(var i=0; i<=9; i++){
+		mark[i] = new Array(9);
+		for(var j=0; j<=8; j++){
+			mark[i][j] = 0;
+		}
+	}
+	mark[0][0] = redRook1;
+	mark[0][1] = redKnight1;
+	mark[0][2] = redBishop1;
+	mark[0][3] = redGuard1;
+	mark[0][4] = redKing;
+	mark[0][5] = redGuard2;
+	mark[0][6] = redBishop2;
+	mark[0][7] = redKnight2;
+	mark[0][8] = redRook2;
+	mark[2][1] = redCannon1;
+	mark[2][7] = redCannon2;
+	mark[3][0] = redPawn1;
+	mark[3][2] = redPawn2;
+	mark[3][4] = redPawn3;
+	mark[3][6] = redPawn4;
+	mark[3][8] = redPawn5;
+	mark[9][0] = blackRook1;
+	mark[9][1] = blackKnight1;
+	mark[9][2] = blackBishop1;
+	mark[9][3] = blackGuard1;
+	mark[9][4] = blackKing;
+	mark[9][5] = blackGuard2;
+	mark[9][6] = blackBishop2;
+	mark[9][7] = blackKnight2;
+	mark[9][8] = blackRook2;
+	mark[7][1] = blackCannon1;
+	mark[7][7] = blackCannon2;
+	mark[6][0] = blackPawn1;
+	mark[6][2] = blackPawn2;
+	mark[6][4] = blackPawn3;
+	mark[6][6] = blackPawn4;
+	mark[6][8] = blackPawn5;
+	for(var i=0; i<=9; i++){
+		for(var j=0; j<=8; j++){
+			drawChess(mark[i][j]);
+		}
+	}
 }
 
-$(function(){
-	calculateArgs();
-	checkerboard();    // 绘制棋盘
-//	chessPiecesInit();
-
-	$(window).resize(function () {
-		calculateArgs();
-		checkerboard();    // 绘制棋盘
-	});
-});
->>>>>>> d6e8097d9caf425263fce2e1633551f5ad80adfa
