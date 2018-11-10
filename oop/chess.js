91ab37732a13e0d6fb4c2677fd671dd24106818d
lightyears <lightyears1998@hotmail.com>
2018-11-10 15:16:43 +0800

    搭建OOP的骨架

--- oop/chess.js
+++ oop/chess.js
@@ -1,55 +1,75 @@
-'use strict'
+'use strict';
+
+var GAME;
 
 // 初始化游戏
 document.addEventListener('DOMContentLoaded', function (event) {
-	createCanvas();
+	GAME = new ChessGame('chess-game');
 });
 
-// 顺序层叠3层Canvas
-function createCanvas() {
-	let node_game = document.getElementById('chess-game-div');
-    if (node_game) {
-        ['cvs-chessboard', 'cvs-chesses', 'cvs-feedback'].forEach(function (cvs_name) {
-            let cvs = document.createElement('canvas');
-            cvs.setAttribute('id', cvs_name);
-            cvs.setAttribute('class', 'chess-game-canvas');
-            node_game.appendChild(cvs);
-        });
-    }
-}
-
-// 棋盘层
-function chessBoard() {
-	this.boarder;  // 棋盘边界
-	this.horizontalLines;  // 棋盘上横线
-	this.cross;            // 棋盘上特殊位置的标记
-	this.verticalLines;    // 棋盘上的水平线
-}
-
-// 棋子层
-function chesss() {
+function ChessGameCanvas(game) {
+	this.cvs_chessboard;
+	this.cvs_chesses;
+	this.cvs_feedback;
 	
-}
-
-// 反馈层
-function Feedback() {
-	
+	// 初始化工作
+	{
+		['cvs-chessboard', 'cvs-chesses', 'cvs-feedback'].forEach(function (cvs_name) {
+			let cvs = document.createElement('canvas');
+			cvs.setAttribute('id', `${game.node.id}-${cvs_name}`);
+			cvs.setAttribute('style', 'position: absolute; width: 100%; height: 100%;');
+			game.node.appendChild(cvs);
+		});
+		this.cvs_chessboard = document.getElementById(`${game.node.id}-cvs-chessboard`);
+		this.cvs_chesses = document.getElementById(`${game.node.id}-cvs-chesses`);
+		this.cvs_feedback = document.getElementById(`${game.node.id}-cvs-feedback`);
+	}
 }
 
 // 象棋游戏对象
-function chessGame() {
-	if (!(this instanceof chessGame)) {
-		return new chessGame();
+function ChessGame(node_id) {
+	if (!(this instanceof ChessGame)) {
+		return new ChessGame(node_id);
 	}
 	
-	this.cell;     // 单位格子的长度
+	// 游戏在DOM树中的节点
+	this.node;
+	
+	// 游戏画布
+	this.canvas;
+	
+	// 初始化工作
+	{
+		this.node = document.getElementById(node_id);
+		if (this.node) {
+			this.canvas = new ChessGameCanvas(this);  // 创建棋盘层、棋子层和反馈层画布
+		} else {
+			throw new Error('人生充满意外，需要放平心态');
+		}
+	}
+	
+	// 初始化游戏
+	this.init;
+	
+	// 开始游戏
+	this.start;
+	
+	// 暂停游戏
+	this.pause;
+	
+	// 结束游戏
+	this.stop;
 	
 	// 将逻辑坐标转换为像素坐标
+	// 参数 {x, y}
+	// 返回 {x, y}
 	this.logicCoordinate2PixelCoordinate = function () {
 		
 	}
 	
 	// 将像素坐标转换为逻辑坐标
+	// 参数 {x, y}
+	// 返回 {x, y}
 	this.PixelCoordinate2LogicCoordinate = function () {
 		
 	}
