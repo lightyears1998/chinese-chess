83812912e24d31f09e2b27fc6535f9ae9073170c
lightyears <lightyears1998@hotmail.com>
2018-11-10 16:58:12 +0800

    重构与逐渐明晰的大纲

--- oop/chess.js
+++ oop/chess.js
@@ -2,75 +2,94 @@
 
 var GAME;
 
+
 // 初始化游戏
 document.addEventListener('DOMContentLoaded', function (event) {
-	GAME = new ChessGame('chess-game');
+	GAME = new ChessGame(document.getElementById('chess-game'));
 });
 
-function ChessGameCanvas(game) {
-	this.cvs_chessboard;
-	this.cvs_chesses;
-	this.cvs_feedback;
-	
-	// 初始化工作
-	{
-		['cvs-chessboard', 'cvs-chesses', 'cvs-feedback'].forEach(function (cvs_name) {
-			let cvs = document.createElement('canvas');
-			cvs.setAttribute('id', `${game.node.id}-${cvs_name}`);
-			cvs.setAttribute('style', 'position: absolute; width: 100%; height: 100%;');
-			game.node.appendChild(cvs);
-		});
-		this.cvs_chessboard = document.getElementById(`${game.node.id}-cvs-chessboard`);
-		this.cvs_chesses = document.getElementById(`${game.node.id}-cvs-chesses`);
-		this.cvs_feedback = document.getElementById(`${game.node.id}-cvs-feedback`);
-	}
-}
 
 // 象棋游戏对象
-function ChessGame(node_id) {
-	if (!(this instanceof ChessGame)) {
-		return new ChessGame(node_id);
+function ChessGame(node) {
+    if (!(this instanceof ChessGame)) {
+    	return new ChessGame(node);
+    }
+    if (!node) {
+    	throw new Error('人生充满意外，需要放平心态');
+    }
+    
+	this.node;   // 游戏在DOM树中的节点
+	this.canvas; // 游戏画布
+
+	this.init;   // 初始化游戏
+	this.start;  // 开始游戏
+	this.pause;  // 暂停游戏
+	this.stop;   // 结束游戏
+
+    this.node = node;
+    this.canvas = new ChessGameCanvas(this);
+}
+
+
+// 绑定游戏对象的画布
+function ChessGameCanvas(game) {
+	if (!(this instanceof ChessGameCanvas)) {
+		return new ChessGameCanvas(game);
 	}
-	
-	// 游戏在DOM树中的节点
-	this.node;
-	
-	// 游戏画布
-	this.canvas;
-	
-	// 初始化工作
-	{
-		this.node = document.getElementById(node_id);
-		if (this.node) {
-			this.canvas = new ChessGameCanvas(this);  // 创建棋盘层、棋子层和反馈层画布
-		} else {
-			throw new Error('人生充满意外，需要放平心态');
-		}
+	if (!(game instanceof ChessGame)) {
+        throw new Error('人生充满意外，需要放平心态');
 	}
-	
-	// 初始化游戏
-	this.init;
-	
-	// 开始游戏
-	this.start;
-	
-	// 暂停游戏
-	this.pause;
-	
-	// 结束游戏
-	this.stop;
-	
-	// 将逻辑坐标转换为像素坐标
-	// 参数 {x, y}
-	// 返回 {x, y}
-	this.logicCoordinate2PixelCoordinate = function () {
-		
-	}
-	
-	// 将像素坐标转换为逻辑坐标
-	// 参数 {x, y}
-	// 返回 {x, y}
-	this.PixelCoordinate2LogicCoordinate = function () {
-		
-	}
+    
+    this.cvsChessboard;   // 棋盘画布
+	this.cvsChesses;      // 棋子画布
+	this.cvsFeedback;     // 反馈画布
+
+    this.updateMeasuring; // 更新度量
+    this.offsetTop;       // 绘图坐标原点距离父元素顶端的偏移
+    this.offsetLeft;      // 绘图坐标原点距离父元素左端的偏移
+    this.cellLength;      // 单位棋盘格子的长度
+
+    this.draw;            // 绘制游戏
+    this.drawChessboard;  // 绘制棋盘
+    this.drawChesses;     // 绘制棋盘上的棋子
+
+    // 棋子逻辑坐标与绘图坐标的相互转换
+	this.logicCoordinate2DrawingCoordinate;
+	this.DrawingCoordinate2LogicCoordinate;
+    
+    this.updateLength = function () {
+        let parentHeight = game.node.clientHeight;
+        let parentWidth = game.node.clientWidth;
+        let availableLength = Math.min(parentHeight, parentWidth);
+        this.cellLengh = availableLength / 11;
+        this.offsetTop = (parentHeight - availableLength) / 2;
+        this.offsetLeft = (parentWidth - availableLength) / 2;
+    }
+    
+    this.draw = function () {
+        this.drawChessboard();
+        this.drawChesses();
+    }
+    
+    this.drawChessboard = function () {
+        
+    }
+    
+    this.drawChesses = function () {
+        
+    }
+    
+    {['cvs-chessboard', 'cvs-chesses', 'cvs-feedback'].forEach(function(cvs_name) {
+    	let cvs = document.createElement('canvas');
+    	cvs.setAttribute('id', `${game.node.id}-${cvs_name}`);
+    	cvs.setAttribute('style', 'position: absolute; width: 100%; height: 100%;');
+    	game.node.appendChild(cvs);
+    });}
+    this.cvsChessboard = document.getElementById(`${game.node.id}-cvs-chessboard`); 
+    this.cvsChesses = document.getElementById(`${game.node.id}-cvs-chesses`); 
+    this.cvsFeedback = document.getElementById(`${game.node.id}-cvs-feedback`);
+    this.updateLength();
+    this.draw();
+   
+    window.addEventListener('resize', this.updateLength.bind(this));
 }
