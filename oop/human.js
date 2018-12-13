bba50cb6f5e5830bc88fb0b6e9a5e761cfa86a18
lightyears <lightyears1998@hotmail.com>
2018-12-13 17:21:40 +0800

    统一缩进格式

--- oop/human.js
+++ oop/human.js
@@ -1,16 +1,15 @@
 'use strict';
 
-
 // 使用本地计算机的玩家
 function LocalPlayer(game) {
-	if (!(this instanceof LocalPlayer)) {
-		return new LocalPlayer(game);
-	}
+  if (!(this instanceof LocalPlayer)) {
+    return new LocalPlayer(game);
+  }
   
   this.lastClickedChess = null;  // 鼠标上次点击的棋子
   
   this.mouseInput = function (event) {
-    let clickedObj = game.canvas.getChess(event.clientX, event.clientY));
+    let clickedObj = game.canvas.getChess(event.clientX, event.clientY);
     if (clickedObj instanceof Chess) {
       if (lastClickedChess) {
         
